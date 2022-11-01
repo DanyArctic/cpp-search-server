e5e5565b377ca4fb8616f17d8ee16d3f2efaa972
DanyArctic <flip4477@gmail.com>
2022-11-01 23:39:28 +0300

    first commit in praticum repo, solved task of finding nums with digits three

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,28 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include <iostream>
+
+bool number_contains_three(const int num)
+{
+    bool answer = false;
+    if (num % 10 == 3 || num / 10 % 10 == 3 || num / 100 == 3)
+    {
+        answer = true;
+    }
+    return answer;
+}
+
+int main()
+{
+    int cnt = 0;
+    for (int i = 1; i <= 1000; i++)
+    {
+        if (number_contains_three(i))
+        {
+            cnt++;
+        }
+    }
+    std::cout << cnt << std::endl;
+    return 0;
+}// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
+// Напишите ответ здесь: 271
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
